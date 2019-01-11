fd17fe2d4c3fc8ac9455da8f7109dc9209289dc9
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-01-11 18:11:52 +0100

    conversioni in hertz

--- hertz.c
+++ hertz.c
@@ -6,54 +6,51 @@ L'algoritmo si adatta ad ogni grandezza fisica (metro,litro ecc...) */
 int main () {
 int numero,conversione,potenza;
 char grandezza[capienza];
-printf ("inserisci l'unità di misura di partenza");
+printf ("inserisci l'unità di misura di partenza \t");
 scanf ("%s",&grandezza);
-printf ("inserisci il valore");
+printf ("inserisci il valore in hertz \t ");
 scanf ("%d",&numero);
-if (grandezza[0]=='g' || grandezza [0] == 'G') {
+if (grandezza[0]== 'G') {
 	potenza=pow(10,9);
 	conversione= numero*potenza;
 }
-else if (grandezza[0]=='m' || grandezza[0]== 'M') {
+else if (grandezza[0]== 'M') {
 	potenza= pow (10,6);
 	conversione= numero*potenza;
 }
-else if (grandezza[0]=='k' || grandezza[0]=='K') {
+else if (grandezza[0]=='K') {
 	potenza= pow (10,3);
 	conversione= numero*potenza;
 }
-else if (grandezza[0]=='e' || grandezza[0]=='E') {
+else if (grandezza[0]=='E') {
 	potenza= pow (10,2);
 	conversione= numero*potenza;
 }
-else if (grandezza[0]=='d' || grandezza[0]=='D' ) {
+else if (grandezza[0]=='D' ) {
 	potenza= pow (10,1);
 	conversione= numero*potenza;
 }
-else if (grandezza[0]=='d' || grandezza[0]== 'D') {
+else if (grandezza[0]=='d') {
 	potenza= pow (10,-3);
 	conversione= numero*potenza;
 }
-else if (grandezza[0]=='c' || grandezza[0]=='C') {
+else if (grandezza[0]=='c') {
 	potenza= pow (10,-2);
 	conversione= numero*potenza;
 }
-else if (grandezza[0]=='m' || grandezza[0]== 'M') {
+else if (grandezza[0]== 'm') {
 	potenza= pow (10,-1);
 	conversione= numero*potenza;
 }
-else if (grandezza[0]=='u' || grandezza[0]=='M' || grandezza[0]=='m') {
-	if (grandezza[1]=='i' || grandezza[1]=='I') {
+else if (grandezza[0]=='u') {
 	potenza= pow (10,-3);
 	conversione= numero*potenza;
 }
- }
-else if (grandezza[0]=='n' || grandezza[0]=='N') {
+else if (grandezza[0]=='n') {
 	potenza= pow (10,-6);
 	conversione= numero*potenza;
 } 
-printf ("la conversione in %s",grandezza);
-printf (" e' %d", conversione);
+printf ("la conversione in %s e' %d",grandezza,conversione);
 return 0;
 }
   
